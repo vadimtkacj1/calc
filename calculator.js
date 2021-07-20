6d0d9316ce245bf45d947e234518abdd47b45c81
Вадим <vadim.tkach1378@gmail.com>
2021-07-20 18:10:48 +0300

    Решения ошибок

--- calculator.js
+++ calculator.js
@@ -4,42 +4,40 @@ function createCalculator() {
   let a = +prompt("Введите 1 число, для подсчета", "0");
   let b = prompt("Введите знак(+, -, *, /, **)", "+");
   let c = +prompt("Введите 2 число, для подсчета", "0");
-
-function calcSum(a, c) {
-	
-  if (b == "+") {
-    let sum = a + c;
-    alert(sum);
-  } else if (b == "-") {
-    let sum = a - c;
-    alert(sum); 
-  } else if (b == "*") {
-    let sum = a * c;
-    alert(sum);
-  } else if (b == "/") {
-    let sum = a / c;
-    alert(sum);
-  } else if (b == "**") {
-    let sum = a ** c;
-    alert(sum);
-  } else {
-	 return alert("Вы ввели неправельный знак !!!");
-  }
   
-  if (isNaN(a) || isNaN(c)) {
-    alert("Вы ввели не цыфру !!!");
+  if (!isFinite(a) || !isFinite(c)) {
+    alert("Вы ввели не цифру !!!");
+    return createCalculator();
   } 
-  
-  let repeatMessage = confirm("Вы хотите ещё что-то посчитать?");
- 
-  if (repeatMessage == true) {
-	return createCalculator();
-  } else if (repeatMessage == false) {
-     alert("Спасибо что пользуетесь нашим калькулятором !!!")
-  }
-}
 
-  return calcSum(a, c);
+  function calcSum(a, b, c) {
+    let operators = {
+      '+': a + c,
+      '-': a - c,
+      '*': a * c,
+      '/': a / c,
+      '**': a ** c,
+    }
+     
+    let sum = operators[b];
+    
+    if(sum === undefined) {
+      alert('Вы ввели не оператор !!!');
+      return createCalculator();
+    } else {
+      alert(sum);
+    }
+                
+    let repeatMessage = confirm("Вы хотите ещё что-то посчитать?");
+
+    if (repeatMessage == true) {
+	    return createCalculator();
+    } else if (repeatMessage == false) {
+      return alert("Спасибо что пользуетесь нашим калькулятором !!!")
+    }
+  }
+
+  return calcSum(a, b, c);
 }
 
 createCalculator();
